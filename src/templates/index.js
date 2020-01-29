47dc3f0698d3a814dd950376b6effacb2225b26d
ilcm96 <ilcm96@gmail.com>
2020-01-29 12:23:55 +0900

    Clean code

--- src/templates/index.js
+++ src/templates/index.js
@@ -71,7 +71,6 @@ export const pageQuery = graphql`
           fields {
             slug
           }
-          timeToRead
           frontmatter {
             date(formatString: "YYYY-MM-DD")
             title
