990b6499c5d0a706f5e957e66541992148fff328
ilcm96 <ilcm96@gmail.com>
2020-01-29 12:30:48 +0900

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+
 import Layout from '../components/Layout';
+import SEO from '../components/SEO';
 
 class NotFoundPage extends React.Component {
   render() {
+    const { data } = this.props;
+    const siteTitle = data.site.siteMetadata.title;
+
     return (
-      <Layout location={this.props.location}>
-        <main>
-          <h1>Not Found</h1>
-          <p>I haven’t written this post yet. Will you help me write it?</p>
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/6IJB0aD8gSA"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullscreen
-          />
-          <p>Too doo doo doo doo doo doo doo</p>
-        </main>
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="404: Not Found" />
+        <h1>404</h1>
+        <h2>
+          페이지를 찾을 수 없습니다{' '}
+          <span role="img" aria-label="Sad face">
+            😥
+          </span>
+        </h2>
       </Layout>
     );
   }
 }
 
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
