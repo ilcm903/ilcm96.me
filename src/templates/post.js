0ecd79f329a1d8f0325622f2b204734b430446da
Yun Seongmin <ilcm96@gmail.com>
2020-02-26 23:03:42 +0900

    FIx layout of previous post link

--- src/templates/post.js
+++ src/templates/post.js
@@ -77,6 +77,7 @@ class BlogPostTemplate extends React.Component {
                 justifyContent: 'space-between',
                 listStyle: 'none',
                 padding: 0,
+                margin: 0,
               }}
             >
               <li>
